c4c62b0a1b5dec99709aef982064f4bf0f6c283e
Jose Luis <pepirob>
2023-08-25 13:40:49 +0200

    proyect-complete

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageLayout from '../../components/PageLayout'
+
+function about () {
+    return (
+        <PageLayout title='About'>
+            <div>
+                <h1>About</h1>
+            </div>
+        </PageLayout>
+    )
+}
+
+export default about
---
+++ src/pages/article/[id].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function article () {
+    return (
+        <>
+            <h1>Esto es un artículo...</h1>
+        </>
+    )
+}
+
+export default article
